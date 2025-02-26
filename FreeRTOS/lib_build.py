a75488ce98a554a625e2529f676df1ba5a15be08
AndyCheng <swps9940902@gmail.com>
2025-02-26 18:20:42 +0800

    Enhance lib_build.py to dynamically configure source filters based on CPU type and FPU presence

--- FreeRTOS/lib_build.py
+++ FreeRTOS/lib_build.py
@@ -1,18 +1,58 @@
 from os.path import join, realpath
-Import('env')
+
+Import("env")
 
 
-src_filter = [
-    "+<source/*>",
-    "+<source/portable/memmang/heap_2.c>",
-    "+<source/portable/GCC/ARM_CM4F/*>",
-    "-<source/portable/common/*>",
-]
-
-env.Replace(SRC_FILTER=src_filter)
-env.Append(CPPPATH=[realpath("source/include")])
-env.Append(CPPPATH=[realpath("source/portable/GCC/ARM_CM4F")])
-# FreeRTOSConfig.h is located in the project space, outside of the lib:
-env.Append(CPPPATH=env.get("PROJECT_INCLUDE_DIR", []))
+board_config = env.BoardConfig()
+cpu_type = board_config.get(
+    "build.cpu", ""
+)  # 通常會回傳類似 "cortex-m3"、"cortex-m4"、"cortex-m4f"等
+# mcu_type = board_config.get("build.mcu", "")  # 若你想讀 MCU 類型也可以
 
 
+def has_fpu_by_cpu_type():
+    # 如果是 "cortex-m4f" 或有 "f"（但還要看實際定義），可以直接視為有 FPU
+    return "m4f" in cpu_type or "m7f" in cpu_type
+
+
+def has_fpu_by_flags():
+    cflags = env.get("CCFLAGS", [])
+    for f in cflags:
+        # 簡單判斷，只要有 -mfpu= 或 -mfloat-abi=hard 之類就認定為有 FPU
+        if "-mfpu=" in f:
+            return True
+        if "-mfloat-abi=softfp" in f:
+            return True
+    return False
+
+
+if has_fpu_by_cpu_type() or has_fpu_by_flags():
+    #
+    # 2) 如果偵測有 FPU (ARM_CM4F)
+    #
+    src_filter = [
+        "+<source/*>",
+        "+<source/portable/memmang/heap_4.c>",
+        "-<source/portable/GCC/ARM_CM3/*>",
+        "+<source/portable/GCC/ARM_CM4F/>",
+        "-<source/portable/common/*>",
+    ]
+
+    env.Replace(SRC_FILTER=src_filter)
+    env.Append(CPPPATH=[realpath("source/include")])
+    env.Append(CPPPATH=[realpath("source/portable/GCC/ARM_CM4F")])
+else:
+    #
+    # 3) 否則就用 ARM_CM3 或其他
+    #
+    src_filter = [
+        "+<source/*>",
+        "+<source/portable/memmang/heap_4.c>",
+        "+<source/portable/GCC/ARM_CM3/*>",
+        "-<source/portable/GCC/ARM_CM4F/>",
+        "-<source/portable/common/*>",
+    ]
+
+    env.Replace(SRC_FILTER=src_filter)
+    env.Append(CPPPATH=[realpath("source/include")])
+    env.Append(CPPPATH=[realpath("source/portable/GCC/ARM_CM3")])
