03a858a1038b914034477f8be91c0062289dfadc
AndyCheng <swps9940902@gmail.com>
2025-02-27 14:25:38 +0800

    Enhance lib_build.py to improve FPU detection and adjust source filters for ARM_CM4F and ARM_CM3

--- FreeRTOS/lib_build.py
+++ FreeRTOS/lib_build.py
@@ -4,14 +4,16 @@ Import("env")
 
 
 board_config = env.BoardConfig()
+
 cpu_type = board_config.get(
     "build.cpu", ""
 )  # 通常會回傳類似 "cortex-m3"、"cortex-m4"、"cortex-m4f"等
 # mcu_type = board_config.get("build.mcu", "")  # 若你想讀 MCU 類型也可以
-
+print("cpu_type =", cpu_type, flush=True)
 
 def has_fpu_by_cpu_type():
     # 如果是 "cortex-m4f" 或有 "f"（但還要看實際定義），可以直接視為有 FPU
+    print(f"Checking FPU from cpu_type: '{cpu_type}'", flush=True)  # 記錄 cpu_type
     return "m4f" in cpu_type or "m7f" in cpu_type
 
 
@@ -30,11 +32,12 @@ if has_fpu_by_cpu_type() or has_fpu_by_flags():
     #
     # 2) 如果偵測有 FPU (ARM_CM4F)
     #
+    print("Using ARM_CM4F", flush=True)
     src_filter = [
         "+<source/*>",
         "+<source/portable/memmang/heap_4.c>",
         "-<source/portable/GCC/ARM_CM3/*>",
-        "+<source/portable/GCC/ARM_CM4F/>",
+        "+<source/portable/GCC/ARM_CM4F/*>",
         "-<source/portable/common/*>",
     ]
 
@@ -44,12 +47,13 @@ if has_fpu_by_cpu_type() or has_fpu_by_flags():
 else:
     #
     # 3) 否則就用 ARM_CM3 或其他
-    #
+    
+    print("Using ARM_CM3", flush=True)
     src_filter = [
         "+<source/*>",
         "+<source/portable/memmang/heap_4.c>",
         "+<source/portable/GCC/ARM_CM3/*>",
-        "-<source/portable/GCC/ARM_CM4F/>",
+        "-<source/portable/GCC/ARM_CM4F/*>",
         "-<source/portable/common/*>",
     ]
 
